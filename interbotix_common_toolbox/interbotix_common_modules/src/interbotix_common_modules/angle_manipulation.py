78a57269d83614c005406c6d84d78af487dbd197
Luke Schmitt <luke@trossenrobotics.com>
2022-02-17 15:45:38 -0600

    [common_modules] Deprecate geometry module
    Relocate methods from the geometry module to angle_manipulation. Add
    required imports to angle_manipulation. Add deprecation warning to
    geometry module. Add exec_depend tag to common_modules package in order
    to require a numpy version greater than 1.20.0, where the identity
    method's like parameter was introduced.

--- interbotix_common_toolbox/interbotix_common_modules/src/interbotix_common_modules/angle_manipulation.py
+++ interbotix_common_toolbox/interbotix_common_modules/src/interbotix_common_modules/angle_manipulation.py
@@ -3,6 +3,8 @@
 import numpy as np
 import math
 
+from geometry_msgs.msg import Quaternion, QuaternionStamped
+
 # Inverts a homogeneous transformation matrix
 def transInv(T):
     R, p = T[:3,:3], T[:3, 3]
@@ -75,3 +77,28 @@ def rotationMatrixToEulerAngles(R):
         z = 0
 
     return np.array([x, y, z])
+
+def quaternion_is_valid(quat, tol=10e-3):
+    """Tests if a quaternion is valid
+    
+    :param quat: Quaternion to check validity of
+    :type quat: geometry_msgs.msg.Quaternion
+    :param tol: tolerance with which to check validity
+    :tpe tol: float
+    :return: `True` if quaternion is valid, `False` otherwise
+    :rtype: bool
+    """
+    if isinstance(quat, Quaternion):
+        return abs((quat.w * quat.w
+            + quat.x * quat.x
+            + quat.y * quat.y
+            + quat.z * quat.z) - 1.0) < tol
+    elif isinstance(quat, QuaternionStamped):
+        return abs((quat.quaternion.w * quat.quaternion.w
+            + quat.quaternion.x * quat.quaternion.x
+            + quat.quaternion.y * quat.quaternion.y
+            + quat.quaternion.z * quat.quaternion.z) - 1.0) < tol
+    else:
+        raise TypeError(
+            ("quaternion_is_valid must be given a Quaternion or "
+            "QuaternionStamped message. Was given %s." % type(quat)))
