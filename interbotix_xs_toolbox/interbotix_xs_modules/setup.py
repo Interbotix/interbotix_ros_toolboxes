527802b81382b7374e7acc16d8497674964f30ee
Luke Schmitt <luke@trossenrobotics.com>
2022-03-15 15:22:35 -0500

    [xs_modules] Restructure package for galactic

--- interbotix_xs_toolbox/interbotix_xs_modules/setup.py
+++ interbotix_xs_toolbox/interbotix_xs_modules/setup.py
@@ -1,12 +1,24 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+package_name = "interbotix_xs_modules"
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=['interbotix_xs_modules'],
-    package_dir={'': 'src'},
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages",
+            [f"resource/{package_name}"]
+        ),
+        (f"share/{package_name}",
+            ["package.xml"]
+        ),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Luke Schmitt",
+    maintainer_email="luke@trossenrobotics.com",
+    description="The interbotix_xs_modules package",
+    license="BSD",
+    entry_points={},
 )
-
-setup(**setup_args)
