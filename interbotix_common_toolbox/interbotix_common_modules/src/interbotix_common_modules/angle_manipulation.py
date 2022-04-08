137f93929c5c28c5481919177cd9cdefb43815ce
Luke Schmitt <luke@trossenrobotics.com>
2022-04-07 20:43:30 -0500

    [common_modules] Use tf transformations
    Instead of writing our own angle manipulation functions, we will instead
    use the tf package's transformations library.

--- interbotix_common_toolbox/interbotix_common_modules/src/interbotix_common_modules/angle_manipulation.py
+++ interbotix_common_toolbox/interbotix_common_modules/src/interbotix_common_modules/angle_manipulation.py
@@ -1,9 +1,8 @@
-# Majority of code copied from https://www.learnopencv.com/rotation-matrix-to-euler-angles/
-
 import numpy as np
 import math
 
 from geometry_msgs.msg import Quaternion, QuaternionStamped
+from tf.transformations import euler_matrix, euler_from_matrix
 
 # Inverts a homogeneous transformation matrix
 def transInv(T):
@@ -25,58 +24,21 @@ def poseToTransformationMatrix(pose):
     mat[:3, 3] = pose[:3]
     return mat
 
-# Calculates Rotation Matrix given euler angles.
 def eulerAnglesToRotationMatrix(theta):
+    """Calculates rotation matrix given euler angles in 'xyz' sequence
+    
+    :param theta: list of 3 euler angles
+    :return: 3x3 rotation matrix equivalent to the given euler angles
+    """
+    return euler_matrix(theta[0], theta[1], theta[2], axes="sxyz")[:3, :3]
 
-    R_x = np.array([[1,         0,                  0                   ],
-                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
-                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
-                    ])
-
-    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
-                    [0,                     1,      0                   ],
-                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
-                    ])
-
-    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
-                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
-                    [0,                     0,                      1]
-                    ])
-
-    R = np.dot(R_z, np.dot( R_y, R_x ))
-
-    return R
-
-# Checks if a matrix is a valid rotation matrix.
-def isRotationMatrix(R):
-    Rt = np.transpose(R)
-    shouldBeIdentity = np.dot(Rt, R)
-    I = np.identity(3)
-    n = np.linalg.norm(I - shouldBeIdentity)
-    return n < 1e-6
-
-
-# Calculates rotation matrix to euler angles
-# The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
 def rotationMatrixToEulerAngles(R):
-    R = np.asarray(R)
-    assert(isRotationMatrix(R))
+    """Calculates euler angles given rotation matrix in 'xyz' sequence
 
-    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
-
-    singular = sy < 1e-6
-
-    if  not singular :
-        x = math.atan2(R[2,1] , R[2,2])
-        y = math.atan2(-R[2,0], sy)
-        z = math.atan2(R[1,0], R[0,0])
-    else :
-        x = math.atan2(-R[1,2], R[1,1])
-        y = math.atan2(-R[2,0], sy)
-        z = 0
-
-    return np.array([x, y, z])
+    :param R: 3x3 rotation matrix
+    :return: list of three euler angles equivalent to the given rotation matrix
+    """
+    return list(euler_from_matrix(R, axes="sxyz"))
 
 def quaternion_is_valid(quat, tol=10e-3):
     """Tests if a quaternion is valid
