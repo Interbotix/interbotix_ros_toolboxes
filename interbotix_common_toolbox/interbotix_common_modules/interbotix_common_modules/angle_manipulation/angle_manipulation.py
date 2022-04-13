86552243865e10f192154ff08fe9b42f72e43972
Luke Schmitt <luke@trossenrobotics.com>
2022-04-13 12:41:07 -0500

    [common_modules] angle_manipulation improvements
    Modifies some of the angle_manipulation functions to use
    tf_transformations instead of custom-written functions. Better document
    functions and add type annotations. Adds tf_transformations and numpy to
    the depends of the common_modules package.xml.

--- interbotix_common_toolbox/interbotix_common_modules/interbotix_common_modules/angle_manipulation/angle_manipulation.py
+++ interbotix_common_toolbox/interbotix_common_modules/interbotix_common_modules/angle_manipulation/angle_manipulation.py
@@ -1,92 +1,72 @@
-# Majority of code copied from https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+"""
+A small library of functions to convert Euler angles to rotation matrices and vice versa
+"""
 
+from typing import List, Union
 import numpy as np
 import math
 
 from geometry_msgs.msg import Quaternion, QuaternionStamped
+from tf_transformations import euler_matrix, euler_from_matrix
 
-# Inverts a homogeneous transformation matrix
-def transInv(T):
+def transInv(T: np.ndarray) -> np.ndarray[np.float32]:
+    """Inverts a homogeneous transformation matrix
+    
+    :param T: The homogeneous transformation matrix to invert
+    :return: The inverted homogeneous transformation matrix
+    """
     R, p = T[:3,:3], T[:3, 3]
     Rt = np.array(R).T
     return np.r_[np.c_[Rt, -np.dot(Rt, p)], [[0, 0, 0, 1]]]
 
-# Calculates 2D Rotation Matrix given a desired yaw angle
-def yawToRotationMatrix(yaw):
+def yawToRotationMatrix(yaw: float) -> np.ndarray[np.float32]:
+    """Calculates 2D Rotation Matrix given a desired yaw angle
+    
+    :param yaw: A yaw rotation
+    :return: A 2x2 rotation matrix rotated yaw radians
+    """
     R_z = np.array([[math.cos(yaw),    -math.sin(yaw)],
                     [math.sin(yaw),    math.cos(yaw)],
                     ])
     return R_z
 
-# Transform a Six Element Pose vector to a Transformation Matrix
-def poseToTransformationMatrix(pose):
+def poseToTransformationMatrix(pose: List[float]) -> np.ndarray[np.float32]:
+    """Transform a Six Element Pose vector to a Transformation Matrix
+    
+    :param pose: A list of 6 floats in the format [x, y, z, roll, pitch, yaw]
+    :return: A 4x4 homogeneous transformation matrix
+    """
     mat = np.identity(4)
     mat[:3, :3] = eulerAnglesToRotationMatrix(pose[3:])
     mat[:3, 3] = pose[:3]
     return mat
 
-# Calculates Rotation Matrix given euler angles.
-def eulerAnglesToRotationMatrix(theta):
+def eulerAnglesToRotationMatrix(theta: List[float]) -> np.ndarray[np.float32]:
+    """Calculates rotation matrix given euler angles in 'xyz' sequence
+    
+    :param theta: list of 3 euler angles
+    :return: 3x3 rotation matrix equivalent to the given euler angles
+    """
+    return euler_matrix(theta[0], theta[1], theta[2], axes="sxyz")[:3, :3]
 
-    R_x = np.array([[1,         0,                  0                   ],
-                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
-                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
-                    ])
+def rotationMatrixToEulerAngles(R) -> List[float]:
+    """Calculates euler angles given rotation matrix in 'xyz' sequence
 
-    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
-                    [0,                     1,      0                   ],
-                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
-                    ])
+    :param R: 3x3 rotation matrix
+    :return: list of three euler angles equivalent to the given rotation matrix
+    """
+    return list(euler_from_matrix(R, axes="sxyz"))
 
-    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
-                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
-                    [0,                     0,                      1]
-                    ])
-
-    R = np.dot(R_z, np.dot( R_y, R_x ))
-
-    return R
-
-# Checks if a matrix is a valid rotation matrix.
-def isRotationMatrix(R):
-    Rt = np.transpose(R)
-    shouldBeIdentity = np.dot(Rt, R)
-    I = np.identity(3, like=R)
-    n = np.linalg.norm(I - shouldBeIdentity)
-    return n < 1e-6
-
-
-# Calculates rotation matrix to euler angles
-# The result is the same as MATLAB except the order
-# of the euler angles ( x and z are swapped ).
-def rotationMatrixToEulerAngles(R):
-    R = np.asarray(R)
-    assert(isRotationMatrix(R))
-
-    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
-
-    singular = sy < 1e-6
-
-    if  not singular :
-        x = math.atan2(R[2,1] , R[2,2])
-        y = math.atan2(-R[2,0], sy)
-        z = math.atan2(R[1,0], R[0,0])
-    else :
-        x = math.atan2(-R[1,2], R[1,1])
-        y = math.atan2(-R[2,0], sy)
-        z = 0
-
-    return np.array([x, y, z])
-
-def quaternion_is_valid(quat, tol=10e-3):
+def quaternion_is_valid(
+    quat: Union[Quaternion, QuaternionStamped],
+    tol: float = 10e-3
+) -> bool:
     """Tests if a quaternion is valid
     
     :param quat: Quaternion to check validity of
-    :type quat: geometry_msgs.msg.Quaternion
     :param tol: tolerance with which to check validity
-    :tpe tol: float
     :return: `True` if quaternion is valid, `False` otherwise
-    :rtype: bool
+    :raises: `TypeError` if quat is not a valid type
     """
     if isinstance(quat, Quaternion):
         return abs((quat.w * quat.w
