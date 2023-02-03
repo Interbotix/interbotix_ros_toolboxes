521c8aeb387de7bd88ea29852d7ccbb05f3a42dd
Luke Schmitt <luke@trossenrobotics.com>
2023-02-03 16:32:40 -0600

    [landmark_modules] Update tests to match changes

--- interbotix_common_toolbox/interbotix_landmark_modules/test/test_landmark.py
+++ interbotix_common_toolbox/interbotix_landmark_modules/test/test_landmark.py
@@ -28,10 +28,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-
-from asyncio import Future
-import unittest
-
 from geometry_msgs.msg import Transform
 from interbotix_landmark_modules.landmark import Landmark, LandmarkCollection
 from launch import LaunchDescription
@@ -44,8 +40,9 @@ import launch_testing.markers
 import pytest
 import rclpy
 from rclpy.executors import SingleThreadedExecutor
+from rclpy.task import Future
 from rclpy.time import Time
-from tf2_geometry_msgs import TransformStamped # noqa
+from tf2_geometry_msgs import TransformStamped
 import tf2_ros
 
 
@@ -85,7 +82,7 @@ def generate_test_description():
             ),
             TimerAction(
                 actions=[ReadyToTest()],
-                period=5.0,
+                period=3.0,
             ),
         ]),
         {
@@ -95,7 +92,7 @@ def generate_test_description():
     )
 
 
-class LandmarkTest(unittest.TestCase):
+class LandmarkTest:
 
     @classmethod
     def setUpClass(cls):
@@ -122,142 +119,97 @@ class LandmarkTest(unittest.TestCase):
         self.node.destroy_node()
 
     def test_label(self):
-        """Test landmark.get_label()."""
         label = 'test'
         lm = Landmark(label, 0, node_inf=self.node)
-        self.assertEqual(
-            label,
-            lm.get_label(),
-            'Landmark label is incorrect.'
-        )
+        assert label == lm.label
+
+        new_label = 'new'
+        lm.label = new_label
+        assert new_label == lm.label
 
     def test_id(self):
-        """Test landmark.get_id()."""
-        id_num = 1
-        lm = Landmark('test', 0, node_inf=self.node)
-        lm.set_id(id_num)
-        self.assertEqual(
-            id_num, lm.get_id(),
-            'Landmark ID is incorrect.'
-        )
+        id_num = 0
+        lm = Landmark('test', id_num, node_inf=self.node)
+        assert id_num == lm.tag_id
+
+        # shouldn't be able to change tag_id once set
+        with pytest.raises(AttributeError):
+            lm.tag_id = id_num
 
     def test_landmark_collection_add_pop(self):
-        """Test adding and removing from landmark collection."""
         lc = LandmarkCollection(node_inf=self.node)
-        self.assertTrue(
-            lc.is_empty(),
-            'LandmarkCollection is_empty gives incorrect result.'
-        )
+        assert lc.is_empty()
 
         lc.add_landmark('test', 0)
-        self.assertEqual(
-            0, lc.get_landmark(0).get_id(),
-            "LandmarkCollection add_landmark didn't work."
-        )
+        assert lc.get_landmark(0).tag_id == 0
 
-        lc.remove_landmark(0)
-        self.assertTrue(
-            lc.is_empty(),
-            "LandmarkCollection remove_landmark didn't work."
-        )
+        assert len(lc) == 1
+
+        popped_lm = lc.pop_landmark(0)
+        assert lc.is_empty()
+
+        assert popped_lm.tag_id == 0
 
     def test_set_and_pub_tfs(self):
-        """Test setting, publishing, and transforming."""
+        # Need a zeroed tf between map and landmarks first
+        tf_map_to_lms = TransformStamped()
+        tf_map_to_lms.header.frame_id = 'map'
+        tf_map_to_lms.header.stamp = Time(seconds=1, nanoseconds=0).to_msg()
+        tf_map_to_lms.child_frame_id = 'landmarks'
+        self.tf_broadcaster.sendTransform(tf_map_to_lms)
+        self.executor.spin_once(timeout_sec=0.5)
+
         lc = LandmarkCollection(
-            node_inf=self.node, ros_on=True, tf_buffer=self.tf_buffer, tf_listener=self.tf_listener
+            node_inf=self.node,
+            ros_on=True,
+            tf_buffer=self.tf_buffer,
+            tf_listener=self.tf_listener
         )
         lc.add_landmark('test_1', 1)
 
         tf_1 = Transform()
 
-        lc.get_landmark(1).set_cam_frame_id('landmarks')
-        lc.get_landmark(1).set_tf_wrt_cam(tf_1)
+        lc.get_landmark(1).cam_frame_id = 'landmarks'
+        lc.get_landmark(1).tf_wrt_cam = Transform()
 
-        future_wait_for_tf: Future = self.tf_buffer.wait_for_transform_async(
-            target_frame='map',
-            source_frame='landmarks',
+        assert self.tf_buffer.can_transform(
+            source_frame='map',
+            target_frame='landmarks',
             time=Time(),
         )
 
-        rclpy.spin_until_future_complete(
-            node=self.node,
-            future=future_wait_for_tf,
-            timeout_sec=5.0
-        )
-
-        lc.get_landmark(1).update_tfs(
+        lc.get_landmark(1).update_tf(
             parent_old='landmarks',
             parent_new='map'
         )
 
         lm_1 = lc.get_landmark(1)
 
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().transform.translation.x,
-            tf_1.translation.x,
-            'Landmark get_tf_wrt_cam x is wrong.'
-        )
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().transform.translation.y,
-            tf_1.translation.y,
-            'Landmark get_tf_wrt_cam y is wrong.'
-        )
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().transform.translation.z,
-            tf_1.translation.z,
-            'Landmark get_tf_wrt_cam z is wrong.'
-        )
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().transform.rotation.x,
-            tf_1.rotation.x,
-            'Landmark get_tf_wrt_cam qx is wrong.'
-        )
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().transform.rotation.y,
-            tf_1.rotation.y,
-            'Landmark get_tf_wrt_cam qy is wrong.'
-        )
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().transform.rotation.z,
-            tf_1.rotation.z,
-            'Landmark get_tf_wrt_cam qz is wrong.'
-        )
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().transform.rotation.w,
-            tf_1.rotation.w,
-            'Landmark get_tf_wrt_cam qw is wrong.'
-        )
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().header.frame_id,
-            'landmarks',
-            'Landmark get_tf_wrt_cam frame_id is wrong'
-        )
-        self.assertEqual(
-            lm_1.get_tf_wrt_cam().child_frame_id,
-            'test_1',
-            'Landmark get_tf_wrt_cam child_frame_id is wrong'
-        )
+        assert lm_1.tf_wrt_cam.transform.translation.x == tf_1.translation.x
+        assert lm_1.tf_wrt_cam.transform.translation.y == tf_1.translation.y
+        assert lm_1.tf_wrt_cam.transform.translation.z == tf_1.translation.z
+        assert lm_1.tf_wrt_cam.transform.rotation.x == tf_1.rotation.x
+        assert lm_1.tf_wrt_cam.transform.rotation.y == tf_1.rotation.y
+        assert lm_1.tf_wrt_cam.transform.rotation.z == tf_1.rotation.z
+        assert lm_1.tf_wrt_cam.transform.rotation.w == tf_1.rotation.w
+        assert lm_1.tf_wrt_cam.header.frame_id == 'landmarks'
+        assert lm_1.tf_wrt_cam.child_frame_id == 'test_1'
 
         tf_2 = Transform()
         tf_2.translation.x = 1.0
 
         lc.add_landmark('test_2', 2)
-        lc.get_landmark(2).set_cam_frame_id('landmarks')
-        lc.get_landmark(2).set_tf_wrt_cam(tf_2)
+        lc.get_landmark(2).cam_frame_id = 'landmarks'
+        lc.get_landmark(2).tf_wrt_cam = tf_2
 
-        lc.get_landmark(2).update_tfs(
+        lc.get_landmark(2).update_tf(
             parent_old='landmarks',
             parent_new='map'
         )
 
-        self.executor.spin_once(timeout_sec=1.0)
+        assert lc.get_landmark(2)._tf_set
 
-        self.assertTrue(
-            lc.get_landmark(2).tf_set_,
-            'Landmark tf_set_ was not set to True correctly.'
-        )
-
-        lc.pub_tfs(tag_ids=[2])
+        lc.pub_tfs(tag_ids=2)
 
         future_wait_for_tf2: Future = self.tf_buffer.wait_for_transform_async(
             target_frame='map',
@@ -267,17 +219,15 @@ class LandmarkTest(unittest.TestCase):
 
         rclpy.spin_until_future_complete(
             node=self.node,
+            executor=self.executor,
             future=future_wait_for_tf2,
-            timeout_sec=5.0
+            timeout_sec=0.5,
         )
 
-        self.executor.spin_once(timeout_sec=5.0)
-
-        # tf2_ros.TransformListener(self.tf_buffer, node=self.node)
         tf_map_to_test_2 = self.tf_buffer.lookup_transform(
             'map',
             'test_2',
-            Time()
+            Time(),
         )
 
         pos_x = tf_map_to_test_2.transform.translation.x == tf_2.translation.x
@@ -289,15 +239,5 @@ class LandmarkTest(unittest.TestCase):
         rot_z = tf_map_to_test_2.transform.rotation.z == tf_2.rotation.z
         rot_w = tf_map_to_test_2.transform.rotation.w == tf_2.rotation.w
 
-        self.assertTrue(
-            (pos_x and pos_y and pos_z),
-            'Landmark transform translation is incorrect.'
-        )
-        self.assertTrue(
-            (rot_x and rot_y and rot_z and rot_w),
-            'Landmark transform rotation is incorrect.'
-        )
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert (pos_x and pos_y and pos_z)
+        assert (rot_x and rot_y and rot_z and rot_w)
